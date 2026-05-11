140d1d86a291de09b7b024f8c60faa9491a8f528
Syrine Benamar <syrine.benamar@student.ehb.be>
2026-05-11 19:42:45 +0200

    zoekfunctie toegevoegd

--- src/main.js
+++ src/main.js
@@ -2,9 +2,24 @@ import "./style.css";
 import { getAnime } from "./api.js";
 import { displayAnime } from "./ui.js";
 
+let animeList = [];
+
 const loadData = async () => {
-  const anime = await getAnime();
-  displayAnime(anime);
+  animeList = await getAnime();
+  displayAnime(animeList);
 };
 
 loadData();
+
+// 🔍 ZOEKFUNCTIE
+const searchInput = document.querySelector("#search");
+
+searchInput.addEventListener("input", (e) => {
+  const value = e.target.value.toLowerCase();
+
+  const filtered = animeList.filter(anime =>
+    anime.title.toLowerCase().includes(value)
+  );
+
+  displayAnime(filtered);
+});
